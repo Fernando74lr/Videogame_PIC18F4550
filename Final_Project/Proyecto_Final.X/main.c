5b4b2927632a787eb1a852387d97506b68919d3f
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2020-11-05 11:50:19 -0600

    Added Print and Travel functions in order to display the scenario

--- Final_Project/Proyecto_Final.X/main.c
+++ Final_Project/Proyecto_Final.X/main.c
@@ -1,48 +1,3 @@
-/**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated using MPLAB(c) Code Configurator
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  MPLAB(c) Code Configurator - 3.15.0
-        Device            :  PIC18F4550
-        Driver Version    :  2.00
-    The generated drivers are tested against the following:
-        Compiler          :  XC8 1.35
-        MPLAB             :  MPLAB X 3.20
-*/
-
-/*
-    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
-*/
-
 #include "mcc.h"
 #include "stdio.h"
 #include "stdlib.h"
@@ -53,6 +8,34 @@
  */
 float  ADC_Value;
 unsigned char ADC_Buffer[10];
+unsigned char pos = 0x00;
+unsigned int enem1[]={0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0 };
+unsigned int enem2[]={};
+
+void pintar(unsigned int enem1[]){
+	char posEn = 0x00;
+
+	for (int i = 0; i < 39; i++){
+		if(enem1[i] == 1){
+			while(BusyXLCD());
+        	SetDDRamAddr(posEn);
+        	while(BusyXLCD());
+        	WriteDataXLCD(38);
+		}
+
+		posEn = posEn + 0x01;
+	}	
+}
+
+void recorrer(unsigned int enem1[]){
+    unsigned int enemtemp[]={};
+    for (int i = 1; i < 39; i++){
+        enemtemp[i-1] = enem1[i];
+    }
+    enemtemp[39] = enem1[0];
+    enem1 = enemtemp;
+
+}
 
 void main(void)
 {
@@ -78,13 +61,13 @@ void main(void)
     
     while (1)
     {
-        unsigned char uwu = 0x00;
+        
         
         while(BusyXLCD());
-        SetDDRamAddr(uwu);
+        SetDDRamAddr(pos);
         while(BusyXLCD());
         WriteDataXLCD(62);
-        
+        /*
         while(BusyXLCD());
         SetDDRamAddr(0x01);
         while(BusyXLCD());
@@ -99,14 +82,29 @@ void main(void)
         SetDDRamAddr(0x03);
         while(BusyXLCD());
         WriteDataXLCD(60);
-        
+        */
         ADC_Value = ADC_GetConversion(0);       //ADC
                                                 //VLSB = (Vref+ - Vref-)/(2^b - 1)
                                                 //VLSB = 5/102= 4.88 mV
                                                 //ValueADC = Vin / 4.887 mV = AN0 / 4.887  
         //print ADC value on LCD
-        sprintf(ADC_Buffer, "%.0f%c ", ADC_Value);
-        //
+        //sprintf(ADC_Buffer, "%.0f%c ", ADC_Value);
+        
+        //Subimos y bajamos
+        
+        if  (ADC_Value > 512){
+            if (pos < 0x40){
+                pos=pos+0x40;
+            }
+        }
+        else{
+            if (pos >= 0x40){
+                pos=pos-0x40;
+            }
+        }
+pintar(enem1);
+recorrer(enem1);
+        
         while(BusyXLCD());
         SetDDRamAddr(0x05);
         //
@@ -117,8 +115,13 @@ void main(void)
         //clear display
         while(BusyXLCD());
         WriteCmdXLCD(0b00000001);
+        
+           
     }
 }
+
+
+
 
 /**
  End of File
